c77bbd72e2b4da3e6f9c13b64a9aa3e2ae6d9a1b
devppratik <devppratik@gmail.com>
2021-01-18 02:22:26 +0530

    Initial Project Commit✌

---
+++ README.md
@@ -0,0 +1,21 @@
+## SomeImages - Image Repository
+
+### Summer 2021 - Shopify Developer Intern Challenge Question
+### This is a development build and is not yet production ready
+
+### Usage
+- Put the correct MongoDB Connection string and SendgridAPI Key in the __nodemon.json__ file
+- Run  ``npm run start`` to start your dev server
+
+### Current Features
+- Add Images to the Repository with description and price
+- Sell/Buy Images from the Repository (Payment Integration is not added. Invoice is generated )
+- Manage your images with admin panel
+- Pagination of images
+
+### Planned/ Upcoming Changes
+- Search for an image with text/categories
+- Multiple adding or deletion of images
+- Saving images to a storage service (Currently images are uploaded to the server itself)
+- Payment Integration (Before production build)
+- Better Frontend
---
+++ models/image.js
@@ -0,0 +1,108 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const imageSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Image', imageSchema);
+
+// const mongodb = require('mongodb');
+// const getDb = require('../util/database').getDb;
+
+// class Image {
+//   constructor(title, price, description, imageUrl, id, userId) {
+//     this.title = title;
+//     this.price = price;
+//     this.description = description;
+//     this.imageUrl = imageUrl;
+//     this._id = id ? new mongodb.ObjectId(id) : null;
+//     this.userId = userId;
+//   }
+
+//   save() {
+//     const db = getDb();
+//     let dbOp;
+//     if (this._id) {
+//       // Update the image
+//       dbOp = db
+//         .collection('images')
+//         .updateOne({ _id: this._id }, { $set: this });
+//     } else {
+//       dbOp = db.collection('images').insertOne(this);
+//     }
+//     return dbOp
+//       .then(result => {
+//         console.log(result);
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       });
+//   }
+
+//   static fetchAll() {
+//     const db = getDb();
+//     return db
+//       .collection('images')
+//       .find()
+//       .toArray()
+//       .then(images => {
+//         console.log(images);
+//         return images;
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       });
+//   }
+
+//   static findById(prodId) {
+//     const db = getDb();
+//     return db
+//       .collection('images')
+//       .find({ _id: new mongodb.ObjectId(prodId) })
+//       .next()
+//       .then(image => {
+//         console.log(image);
+//         return image;
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       });
+//   }
+
+//   static deleteById(prodId) {
+//     const db = getDb();
+//     return db
+//       .collection('images')
+//       .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+//       .then(result => {
+//         console.log('Deleted');
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       });
+//   }
+// }
+
+// module.exports = Image;
